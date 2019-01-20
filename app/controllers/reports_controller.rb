f8eeddc14f4cc64c03ac429814c690e61b21e4c8
IZUMIRU <izumiruryousuke@gmail.com>
2019-01-20 15:05:23 +0900

    fix: ogp

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,8 @@
 class ReportsController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :new, :create, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :update, :destroy]
 
   def show
+    @report = Report.find(params[:id])
   end
 
   def start
@@ -15,9 +16,9 @@ class ReportsController < ApplicationController
   def create
     @report = Report.new(report_params)
     if @report.save
-      redirect_to :root
+      redirect_to @report
     else
-      render :set
+      redirect_to new_report_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   root to: 'notifications#top'
-  get 'notifications/set'  => 'notifications#set'
-  resources :notifications,  only: [:create, :update]
+  get 'notifications/set' => 'notifications#set'
+  resources :notifications, only: [:create, :update]
 
-  get 'reports/start'  => 'reports#start'
-  resources :reports,  only: [:show, :new, :create]
-
-  get 'images/ogp.png', to: 'images#ogp'
+  get 'reports/start' => 'reports#start'
+  resources :reports, only: [:show, :new, :create]
 end
