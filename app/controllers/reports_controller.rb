76132f3553b753bce9ef8fd6e79c485353514b7f
IZUMIRU <izumiruryousuke@gmail.com>
2019-02-08 09:09:07 +0900

    feat: 任意のテキストをツイートさせる

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,7 @@
 class ReportsController < ApplicationController
+  require 'twitter'
   before_action :authenticate_user!, only: [:new, :create, :update, :destroy]
+  before_action :set_twitter_client
 
   def show
     @report = Report.find(params[:id])
@@ -16,6 +18,7 @@ class ReportsController < ApplicationController
   def create
     @report = Report.new(report_params)
     if @report.save
+      # @twitter.update(@report.description)
       redirect_to @report
     else
       redirect_to new_report_path
@@ -23,6 +26,15 @@ class ReportsController < ApplicationController
   end
 
   private
+  def set_twitter_client
+    @twitter = Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.credentials.twitter[:api_key]
+      config.consumer_secret     = Rails.application.credentials.twitter[:api_secret]
+      config.access_token        = Rails.application.credentials.twitter[:access_token]
+      config.access_token_secret = Rails.application.credentials.twitter[:access_token_secret]
+    end
+  end
+
   def report_params
     params.require(:report).permit(:description).merge(user_id: current_user.id)
   end
